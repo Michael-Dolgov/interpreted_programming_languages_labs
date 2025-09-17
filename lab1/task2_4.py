9f9303e2a545c353a12f48b3ae68860ea428a723
Michael-Dolgov <michaeldlgov@gmail.com>
2025-09-17 18:11:37 +0300

    task2

--- lab1/task2_4.py
+++ lab1/task2_4.py
@@ -10,3 +10,13 @@ def shuffle_words(text: str) -> str:
     return ' '.join(words)
 
 
+def odd_word_chars_cnt(text: str) -> str:
+    cnt = 0
+    words = text.split(' ')
+    for word in words:
+        if len(word)%2==1:
+            cnt += 1
+    return cnt
+
+
+
