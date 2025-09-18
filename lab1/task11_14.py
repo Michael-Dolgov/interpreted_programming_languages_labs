44d701d6d137de62b20f8a160b9d565f6f52f61f
Michael-Dolgov <michaeldlgov@gmail.com>
2025-09-18 18:10:21 +0300

    task11

---
+++ lab1/task11_14.py
@@ -0,0 +1,39 @@
+import string
+
+def sort_lines_by_char_freq_diff(text: str) -> list[str]:
+    lines = text.splitlines()
+
+    def diff_value(s: str) -> int:
+        freq = {}
+        for ch in s.lower():
+            if ch.isalpha():
+                freq[ch] = freq.get(ch, 0) + 1
+        if not freq:
+            return float("inf")
+        char = max(freq, key=freq.get)
+        freq_in_str = freq[char]
+        pos = string.ascii_lowercase.index(char) + 1
+        return abs(freq_in_str - pos)
+
+    return sorted(lines, key=diff_value)
+
+
+
+def app():
+    choise = input("Выберите задачу 11-14: ")
+    match choise:
+        #3
+        case 11:
+            sort_lines_by_char_freq_diff(input())
+        #5
+        case 12:
+            pass
+        #7
+        case 13:
+            pass
+        #12
+        case 14:
+            pass
+
+if __name__ == '__main__':
+    app()
