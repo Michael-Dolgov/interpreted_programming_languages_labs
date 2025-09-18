b48a9f7076f6c5e5897a399f14045c149ba60b17
Michael-Dolgov <michaeldlgov@gmail.com>
2025-09-18 18:23:08 +0300

    task12

--- lab1/task11_14.py
+++ lab1/task11_14.py
@@ -1,5 +1,6 @@
 import string
 
+
 def sort_lines_by_char_freq_diff(text: str) -> list[str]:
     lines = text.splitlines()
 
@@ -18,6 +19,46 @@ def sort_lines_by_char_freq_diff(text: str) -> list[str]:
     return sorted(lines, key=diff_value)
 
 
+def sort_lines_by_squared_deviation(text: str) -> list[str]:
+    lines = text.splitlines()
+    alphabet = string.ascii_lowercase
+    N = len(alphabet)
+    uniform_freq = 1 / N
+
+    def deviation(s: str) -> float:
+        letters = [ch.lower() for ch in s if ch.isalpha()]
+        if not letters:
+            return float("inf")
+        freq = {}
+        for ch in letters:
+            freq[ch] = freq.get(ch, 0) + 1
+        most_freq_char = max(freq, key=freq.get)
+        f_s = freq[most_freq_char] / len(letters)
+        return (f_s - uniform_freq) ** 2
+
+    return sorted(lines, key=deviation)
+
+
+def sort_by_vc_cv_diff(text: str) -> list[str]:
+    lines = text.splitlines()
+    vowels = set("aeiouAEIOU")
+
+    def vc_cv_diff(s: str) -> int:
+        letters = [ch for ch in s if ch.isalpha()]
+        vc = 0
+        cv = 0
+        for i in range(len(letters) - 1):
+            if letters[i] in vowels and letters[i+1] not in vowels:
+                vc += 1
+            elif letters[i] not in vowels and letters[i+1] in vowels:
+                cv += 1
+        return vc - cv
+
+    return sorted(lines, key=vc_cv_diff)
+
+
+
+
 
 def app():
     choise = int(input("Выберите задачу 11-14: "))
@@ -27,7 +68,7 @@ def app():
             sort_lines_by_char_freq_diff(input("Введите текст: "))
         #5
         case 12:
-            pass
+            sort_lines_by_squared_deviation(input("Введите текст: "))
         #7
         case 13:
             pass
