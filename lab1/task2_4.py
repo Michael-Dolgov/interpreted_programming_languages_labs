8beb91d9ecb42255bcc824b57696987558e535d6
Michael-Dolgov <michaeldlgov@gmail.com>
2025-09-18 17:23:11 +0300

    task2-4

--- lab1/task2_4.py
+++ lab1/task2_4.py
@@ -1,7 +1,17 @@
 from random import shuffle
 
 def app() -> None:
-    ...
+    request = int(input("Выберите метод(1,2,3): "))
+    match request:
+        case 1:
+            text = "biba boba aboba"
+            print(shuffle_words(text))
+        case 2:
+            text = "hello world folks"
+            print(odd_word_chars_cnt(text))
+        case 3:
+            random_colors = ["", "", ""]
+            print(russian_threecolor(random_colors))
 
 
 def shuffle_words(text: str) -> str:
@@ -29,5 +39,3 @@ def russian_threecolor(colors: list[str]) -> list[str]:
             case "красный":
                 colors[2], colors[i] = colors[i], colors[2]
     return colors
-
-
